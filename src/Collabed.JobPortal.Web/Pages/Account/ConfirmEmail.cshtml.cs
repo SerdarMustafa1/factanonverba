d2dc1d758c24cec8ce9114279dac159f0c157865
Mariusz.Budzisz <Mariusz.Budzisz@broadlight.io>
2023-02-24 07:35:31 +0000

    Merged PR 37: Finishing Register Flow
    Related work items: #285

--- src/Collabed.JobPortal.Web/Pages/Account/ConfirmEmail.cshtml.cs
+++ src/Collabed.JobPortal.Web/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,11 +1,17 @@
+using Collabed.JobPortal.Organisations;
+using Collabed.JobPortal.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using System;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Identity;
+using Volo.Abp.Users;
 
 namespace Collabed.JobPortal.Web.Pages.Account
 {
@@ -13,8 +19,17 @@ namespace Collabed.JobPortal.Web.Pages.Account
     public class CustomConfirmEmailModel : PageModel
     {
         private readonly IdentityUserManager _userManager;
+        private readonly IOrganisationRepository _organisationRepository;
+        public bool IsOrganisation { get; set; } = false;
 
-        public CustomConfirmEmailModel(IdentityUserManager userManager) => _userManager = userManager;
+        [BindProperty(SupportsGet = true)]
+        public string DisplayName { get; set; }
+
+        public CustomConfirmEmailModel(IdentityUserManager userManager, IOrganisationRepository organisationRepository)
+        {
+            _userManager = userManager;
+            _organisationRepository = organisationRepository;
+        }
 
         public async Task<IActionResult> OnGetAsync(string userId, string code)
         {
@@ -26,14 +41,24 @@ namespace Collabed.JobPortal.Web.Pages.Account
             {
                 return NotFound($"Unable to load user with ID '{userId}'.");
             }
+            var organisationClaim = (await _userManager.GetClaimsAsync(user)).FirstOrDefault(claim => claim.Type.Equals(ClaimNames.OrganisationClaim));
+
+            if (organisationClaim != null && !string.IsNullOrEmpty(organisationClaim.Value))
+            {
+                var org = await _organisationRepository.GetAsync(o => o.Id.Equals(Guid.Parse(organisationClaim.Value)));
+                DisplayName = org.Name;
+                IsOrganisation = true;
+            }
+            else
+            {
+                DisplayName = user.Name;
+                IsOrganisation = false;
+            }
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             await _userManager.ConfirmEmailAsync(user, code);
 
             return Page();
         }
-
-        // If there is 'Confirm email' button, move ConfirmEmailAsync to OnPostAsync
-        // public async Task<IActionResult> OnPostAsync(string userId, string code)
     }
 }
