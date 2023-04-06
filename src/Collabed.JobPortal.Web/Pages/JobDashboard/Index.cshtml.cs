9942aebbbf9ff822ad05a76ee4a50b76ef3db9c9
Mariusz.Budzisz <Mariusz.Budzisz@broadlight.io>
2023-04-06 13:58:57 +0000

    Merged PR 68: Adding landing page for JobDashboard
    Related work items: #379

--- src/Collabed.JobPortal.Web/Pages/JobDashboard/Index.cshtml.cs
+++ src/Collabed.JobPortal.Web/Pages/JobDashboard/Index.cshtml.cs
@@ -1,12 +1,26 @@
+using Collabed.JobPortal.Jobs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Collabed.JobPortal.Web.Pages.JobDashboard
 {
     public class JobDashboardModel : PageModel
     {
-        public void OnGet()
+        private readonly IJobAppService _jobAppService;
+        public CategorisedJobsDto[] Categories;
+
+        public JobDashboardModel(IJobAppService jobAppService)
         {
+            _jobAppService = jobAppService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Categories = (await _jobAppService.GetCategorisedJobs()).ToArray();
         }
     }
 }
