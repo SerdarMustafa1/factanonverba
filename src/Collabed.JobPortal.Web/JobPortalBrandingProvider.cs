e730205501ac0096f905ab913d419b116c09507c
Mariusz.Budzisz <Mariusz.Budzisz@broadlight.io>
2023-04-14 15:20:00 +0000

    Merged PR 88: change app name
    change app name
    
    Related work items: #402

--- src/Collabed.JobPortal.Web/JobPortalBrandingProvider.cs
+++ src/Collabed.JobPortal.Web/JobPortalBrandingProvider.cs
@@ -6,6 +6,6 @@ namespace Collabed.JobPortal.Web;
 [Dependency(ReplaceServices = true)]
 public class JobPortalBrandingProvider : DefaultBrandingProvider
 {
-    public override string AppName => "Build my Talent";
+    public override string AppName => "BuildMyTalent";
     public override string LogoUrl => "/images/logo/BMT_Primary Full_symbol.png";
 }
